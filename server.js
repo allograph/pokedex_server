918a26c6b77baee3d54a3128ffb7716af771db5b
RachelMinto <rachel.j.minto@gmail.com>
2017-04-06 17:05:27 -0400

    Changed CORS options.

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ const graphqlHTTP = require("express-graphql");
 const app = express();
 var cors = require('cors')
 
-app.use(cors())
+app.use(allowCrossDomain);
+// app.use(cors())
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 
 var port = process.env.PORT || 4000;
 
